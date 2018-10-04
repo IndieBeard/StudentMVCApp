d8048cc0ca202564c46d0017c6cb0a70c00df933
Unknown <andyrobertdev@gmail.com>
2018-10-04 19:14:54 -0400

    Work done during class, (Does not compile)

--- src/basicmvcapp/StudentCntl.java
+++ src/basicmvcapp/StudentCntl.java
@@ -5,9 +5,12 @@ public class StudentCntl {
     private static final int STARTING_INDEX_OF_DISPLAY = 0;
     StudentList studentList;
     StudentUI studentUI;
+    StudentTableModel studentTableModel;
+    
 
     public StudentCntl() {
         studentList = new StudentList();
+        studentTableModel = new StudentTableModel(studentList.getStudentList());
         studentUI = new StudentUI(this, STARTING_INDEX_OF_DISPLAY); // pass 0 so UI starts at first element in list
         studentUI.setVisible(true);
     }
@@ -15,6 +18,10 @@ public class StudentCntl {
     Student getStudent(int index) {
         // you'll need to change this to return the Student item at the given index in StudentList
         return studentList.getStudentList().get(index);
+    }
+    
+    public InstrumentTableModel getInstrumentTableModel(){
+        return studentTableModel;
     }
 
 }
---
+++ src/basicmvcapp/StudentTableModel.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicmvcapp;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andyr
+ */
+public class StudentTableModel extends AbstractTableModel{
+    
+    private String[] columnNames = {"First Name", "Last Name", "GPA"};
+    private ArrayList<Student> studentList;
+    
+    public StudentTableModel(ArrayList<Student> newStudentList){
+        
+        studentList = newStudentList;
+        
+    }
+    
+    
+    @Override
+    public String getColumnName(int col) {
+        return columnNames[col].toString();
+    }
+    
+    public int getRowCount() { return studentList.size(); }
+    
+    @Override
+    public int getColumnCount() { return columnNames.length; }
+    
+    @Override
+    public Object getValueAt(int row, int col) {
+        switch(col){
+            case 0: return (Object) studentList.get(row).getFirstName();
+            case 1: return (Object) studentList.get(row).getLastName();
+            case 2: return (Object) studentList.get(row).getGpa();
+            default: return null;
+        }
+    }
+    
+    @Override
+    public boolean isCellEditable(int row, int col)
+        { return true; }
+    
+    @Override
+    public void setValueAt(Object value, int row, int col) {
+        rowData[row][col] = value;
+        fireTableCellUpdated(row, col);
+    }
+    
+}
